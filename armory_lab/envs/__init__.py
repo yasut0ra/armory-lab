f383177f6aab6258114e92baa6deeaaf9df9fe5d
yasut0ra <yastar.tkm83@gmail.com>
2026-02-13 02:01:19 +0900

    環境モジュール内のWeaponDamageBanditおよび関連関数をエクスポートし、バンディットアルゴリズムの機能を強化

--- armory_lab/envs/__init__.py
+++ armory_lab/envs/__init__.py
@@ -1,3 +1,4 @@
 from armory_lab.envs.bernoulli import BernoulliBandit
+from armory_lab.envs.weapon_damage import WeaponDamageBandit, WeaponPack, generate_weapon_pack
 
-__all__ = ["BernoulliBandit"]
+__all__ = ["BernoulliBandit", "WeaponDamageBandit", "WeaponPack", "generate_weapon_pack"]
